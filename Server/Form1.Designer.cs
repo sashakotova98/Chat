71be9055128b4f6cec660a43a650ce08e4469ac1
Sasha Kotova <sashakotova598@gmail.com>
2023-10-30 18:58:35 +0200

    server-winForms

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Server;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        btnStart = new Button();
+        SuspendLayout();
+        // 
+        // btnStart
+        // 
+        btnStart.Location = new Point(273, 142);
+        btnStart.Name = "btnStart";
+        btnStart.Size = new Size(232, 109);
+        btnStart.TabIndex = 0;
+        btnStart.Text = "Start";
+        btnStart.UseVisualStyleBackColor = true;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(10F, 25F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(btnStart);
+        Name = "Form1";
+        Text = "Form1";
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Button btnStart;
+}
